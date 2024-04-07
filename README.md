aeb10dd5a7043a54b70add8fb2839b4e56ee1ed6
Kmolina009 <kevenmolina@gmail.com>
2024-04-06 20:55:36 -0400

    Scraper thus far, when attempting 403 Status code  received. :(

---
+++ README.md
@@ -0,0 +1,2 @@
+A simple webscraper that gathers the lots of a given auction on HA
+Starting with https://fineart.ha.com/c/search/results.zx?dept=1544&mode=live&auction_name=8163&ic=Items-OpenAuctions-ComingSoon-BrowseViewLots-071713
---
+++ scraper.py
@@ -0,0 +1,20 @@
+from bs4 import BeautifulSoup
+import requests
+
+url = "https://fineart.ha.com/c/search/results.zx?dept=1544&mode=live&auction_name=8163&ic=Items-OpenAuctions-ComingSoon-BrowseViewLots-071713"
+response = requests.get(url)
+
+# Check if the request was successful (status code 200)
+if response.status_code == 200:
+    # Parse the HTML content
+    soup = BeautifulSoup(response.content, 'html.parser')
+    # Now you can work with the parsed HTML (soup) object
+    # Find the section containing the artworks
+    # artworks_section = soup.find('section', class_='items-list')
+
+else:
+    print(response.status_code)
+    print("Failed to fetch the URL:", url)
+
+
+# NOTE: Received 403 error
